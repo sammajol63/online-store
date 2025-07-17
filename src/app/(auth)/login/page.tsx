37f0c36578bef96bc572d4b1798bc4bb4dc26913
sammajol63 <samuel96silalahi@gmail.com>
2025-07-18 01:06:51 +0700

    update fallback Loading

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import LoginContent from "./loginContent";
 
 export default function LoginPage() {
   return (
-    <Suspense fallback={<div>Loading Login...</div>}>
+    <Suspense fallback={<div>null</div>}>
       <LoginContent />
     </Suspense>
   );
